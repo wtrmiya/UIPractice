59e8c6095fbc0d83c24a35b3b316b3deee71fd8e
Wataru Miyakoshi <wtrmiya@gmail.com>
2021-11-29 06:43:06 +0900

    Initial Commit

---
+++ UIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPractice
+//
+//  Created by Wataru Miyakoshi on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
