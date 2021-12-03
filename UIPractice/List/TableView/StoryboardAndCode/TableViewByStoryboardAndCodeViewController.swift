f07f5e79e86791384987c06f96f07641910e47cb
Wataru Miyakoshi <wtrmiya@gmail.com>
2021-12-04 05:08:51 +0900

    add: TableView: Storybaord and code
    TableView: Storybaord, セル: コード でリストを実装した。

---
+++ UIPractice/List/TableView/StoryboardAndCode/TableViewByStoryboardAndCodeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TableViewByStoryboardAndCodeViewController.swift
+//  UIPractice
+//
+//  Created by Wataru Miyakoshi on 2021/12/04.
+//
+
+import UIKit
+final class TableViewByStoryboardAndCodeViewController:UIViewController {
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.register(TableViewByStoryboardAndCodeCell.self, forCellReuseIdentifier: TableViewByStoryboardAndCodeCell.reuseIdentiifer)
+        }
+    }
+    
+    private var albums:[Album] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        albums = DataSource.shared.albums
+        
+        tableView.dataSource = self
+    }
+}
+
+extension TableViewByStoryboardAndCodeViewController:UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albums.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewByStoryboardAndCodeCell.reuseIdentiifer, for: indexPath) as? TableViewByStoryboardAndCodeCell
+        else {
+            return UITableViewCell()
+        }
+        
+        let item = albums[indexPath.row]
+        
+        cell.configure(title: item.title, artist: item.artist)
+        
+        return cell
+    }
+}
